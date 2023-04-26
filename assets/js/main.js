71e066beb0d29849d43ebd1be4b78d494e14e5f1
ADRESS06 <yoimarj0660@gmail.com>
2023-04-26 13:22:18 -0500

    pagina Adaptada a movil(aun en revision)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
-creator = "Yoimar julio"
+//agradecimientos
+creator = "Yoimar Andres julio Salcedo"
+creatorC = "Yoimar"
+fotografa = "Sara Negrete"
+console.log(`Esta pagina fue creada por ${creator} con asistencia fotografica de ${fotografa}`);
+alert(`${creatorC} le sugiere visitar la pagina desde un Ordenador`)
 
-console.log(`Esta pagina fue creada por ${creator}`)
+//funcion del burger-menu
+let menu = document.getElementById("menu");
+let mopen = document.getElementById("mopen");
+let mclose = document.getElementById("mclose");
+
+mopen.addEventListener("click", imenu);
+mclose.addEventListener("click", imenu);
+
+function imenu(){
+    menu.classList.toggle("ver-menu");
+
+    if(menu.classList.contains("ver-menu")){
+        mopen.style.display = "none";
+        mclose.style.display = "block";
+    }
+    else{
+        mopen.style.display = "block";
+        mclose.style.display = "none";
+    }
+}
